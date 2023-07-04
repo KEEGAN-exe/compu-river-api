da698071c49192841dae2f24b1adfd3d142a35c4
Keegan <juan789huarcaya@gmail.com>
2023-07-03 20:18:46 -0500

    correcion cors

--- src/app.js
+++ src/app.js
@@ -1,4 +1,3 @@
-import cors from "cors";
 import express from "express";
 import compuriverCompra from "./routes/compuriver.compra.routes.js";
 import compuriverVenta from "./routes/compuriver.venta.routes.js";
@@ -7,7 +6,6 @@ import compuriver from "./routes/compuriver.routes.js";
 const app = express();
 
 app.use(express.json())
-
 app.use("/api", compuriver);
 app.use("/api", compuriverCompra);
 app.use("/api", compuriverVenta);
