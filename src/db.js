7e5dce728d0223f0e0f8a1b384dc9a4a58e42bc8
Keegan <juan789huarcaya@gmail.com>
2023-07-03 20:22:28 -0500

    corrigiendo las variables

--- src/db.js
+++ src/db.js
@@ -1,11 +1,10 @@
 import { createPool } from "mysql2/promise";
-import {DB_HOST,DB_NAME,DB_PASSWORD,DB_PORT,DB_USER} from './config.js'
+import {DB_HOST, DB_USER, DB_PASSWORD, DB_PORT, DB_NAME} from './config.js'
 
-export const pool = createPool({
+export const consulta = createPool({
   host: DB_HOST,
   user: DB_USER,
   password: DB_PASSWORD,
   port: DB_PORT,
-  database: DB_NAME,
-});
-
+  database: DB_NAME
+})
