372280b841e1728d434a1f44bd54188811247300
Keegan <juan789huarcaya@gmail.com>
2023-07-24 14:41:48 -0500

    categoria crud completo

--- src/controller/categoria.controller.js
+++ src/controller/categoria.controller.js
@@ -8,39 +8,49 @@ export const findAllCategory = async (req, res) => {
 export const findCategoryById = async (req, res) => {
   const { id } = req.params;
   const [result] = await consulta.query(
-    "SELECT * FROM categoria WHERE idCategoria = ?"
-  ,[id]);
-  if(result.length === 0){
-    return res.json({message: "No existe la categoria"});
+    "SELECT * FROM categoria WHERE idCategoria = ?",
+    [id]
+  );
+  if (result.length === 0) {
+    return res.json({ message: "No existe la categoria" });
   }
   return res.json(result[0]);
 };
 
 export const createCategory = async (req, res) => {
-  const { nombre } = req.body;
+  const { nombre, image } = req.body;
   const [rusult] = await consulta.query(
-    "INSERT INTO categoria (nombre) VALUES (?)",
-    [nombre]
+    "INSERT INTO categoria (nombre,image) VALUES (?,?)",
+    [nombre, image]
   );
-  res.json({ id: rusult.insertId, nombre });
+  res.json({ id: rusult.insertId, nombre, image });
 };
 
 export const updateCategory = async (req, res) => {
-  const {id} = req.params;
-  const {nombre} = req.body;
-  const [result ] = await consulta.query("UPDATE categoria SET nombre = IFNULL(?,nombre) WHERE idCategoria = ?", [nombre,id]);
-  if(result.affectedRows >= 1){
-    const [update] = await consulta.query("SELECT * FROM categoria WHERE idCategoria = ?", [id]);
+  const { id } = req.params;
+  const { nombre, image } = req.body;
+  const [result] = await consulta.query(
+    "UPDATE categoria SET nombre = IFNULL(?,nombre), image = IFNULL(?,image) WHERE idCategoria = ?",
+    [nombre, image, id]
+  );
+  if (result.affectedRows >= 1) {
+    const [update] = await consulta.query(
+      "SELECT * FROM categoria WHERE idCategoria = ?",
+      [id]
+    );
     return res.json(update[0]);
   }
-  return res.json({message: "No existe la categoria"});
-}
+  return res.json({ message: "No existe la categoria" });
+};
 
 export const deleteCategory = async (req, res) => {
-  const {id} = req.params;
-  const [result] = await consulta.query("DELETE FROM categoria WHERE idCategoria = ?", [id]);
-  if(result.affectedRows >= 1){
-    return res.json({message: "Categoria eliminada"});
+  const { id } = req.params;
+  const [result] = await consulta.query(
+    "DELETE FROM categoria WHERE idCategoria = ?",
+    [id]
+  );
+  if (result.affectedRows >= 1) {
+    return res.json({ message: "Categoria eliminada" });
   }
-  return res.json({message: "No existe la categoria"});
-}
+  return res.json({ message: "No existe la categoria" });
+};
